6793d56531a8a5d1efe8cf5e08fe2dee64eae2bc
mikel <mikelduke@mdp3.net>
2019-03-02 19:42:43 -0600

    add some color changes

--- rocket-lamp.ino
+++ rocket-lamp.ino
@@ -1,12 +1,7 @@
 #define FASTLED_ESP8266_NODEMCU_PIN_ORDER
 #include <FastLED.h>
 
-// How many leds in your strip?
 #define NUM_LEDS 16
-
-// For led chips like Neopixels, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806, define both DATA_PIN and CLOCK_PIN
 #define DATA_PIN 3
 #define CLOCK_PIN 13
 
@@ -16,8 +11,8 @@
 CRGB leds[NUM_LEDS];
 
 void setup() { 
-  Serial.begin(57600);
-  Serial.println("resetting");
+  Serial.begin(115200);
+  Serial.println("Rocket Lamp");
   LEDS.addLeds<WS2812,DATA_PIN,RGB>(leds,NUM_LEDS);
   LEDS.setBrightness(84);
 }
@@ -25,33 +20,48 @@ void setup() {
 void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
 
 void loop() { 
-  static uint8_t hue = 0;
-  Serial.print("x");
-  // First slide the led in one direction
-  for(int i = 0; i < NUM_LEDS; i++) {
-    // Set the i'th led to red 
-    leds[i] = CHSV(hue++, 255, 255);
-    // Show the leds
-    FastLED.show(); 
-    // now that we've shown the leds, reset the i'th led to black
-    // leds[i] = CRGB::Black;
-    fadeall();
-    // Wait a little bit before we loop around and do it again
-    delay(DELAY);
-  }
-  Serial.print("x");
+  huefade();
+}
 
-  // Now go in the other direction.  
-//  for(int i = (NUM_LEDS)-1; i >= 0; i--) {
-  for(int i = 0; i < NUM_LEDS; i++) {
-    // Set the i'th led to red 
+void ring() {
+  static uint8_t hue = 0;
+  Serial.println("ring");
+
+  for(int i = 0; i < NUM_LEDS / 2; i++) {
     leds[i] = CHSV(hue++, 255, 255);
-    // Show the leds
-    FastLED.show();
-    // now that we've shown the leds, reset the i'th led to black
-    // leds[i] = CRGB::Black;
+    leds[i + (NUM_LEDS /2)] = CHSV(hue++, 255, 255);
+    FastLED.show(); 
+    
     fadeall();
-    // Wait a little bit before we loop around and do it again
     delay(DELAY);
   }
+}
+
+void ring_two() {
+  static uint8_t hue = 0;
+  Serial.println("ring_two");
+
+  for(int i = 0; i < NUM_LEDS / 2; i++) {
+    // Set the i'th led to red 
+    leds[i] = CHSV(hue++, 255, 255);
+    leds[i + (NUM_LEDS /2)] = CHSV(hue++, 255, 255);
+    FastLED.show(); 
+
+    leds[i] = CRGB::Black;
+    leds[i + (NUM_LEDS /2)] = CRGB::Black;
+
+    delay(DELAY);
+  }
+}
+
+void huefade() {
+  static uint8_t hue = 0;
+  Serial.println("huefade");
+
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(hue, 255, 255);
+  }
+  hue++;
+  FastLED.show(); 
+  delay(DELAY / 2);
 }
